b5393f0792744c99b69a8a2c42762ee1e4fe4272
Yuri Kozyk <y.kozyk@gmail.com>
2022-08-22 17:53:03 +0300

    add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import Typography from '@mui/material/Typography'
 
 const Header = () => {
-  return <Typography variant="h1">Header</Typography>
+  return (
+    <Typography
+      variant="h4"
+      component="h1"
+      sx={{
+        margin: '2rem 0',
+      }}
+    >
+      Header
+    </Typography>
+  )
 }
 
 export default Header
