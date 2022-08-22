43cd69b034ba8e761570b6269865b858731d532c
Yuri Kozyk <y.kozyk@gmail.com>
2022-08-22 12:43:36 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Step1 from './pages/Step1'
+import Step2 from './pages/Step2'
+import Step3 from './pages/Step3'
+import Step4 from './pages/Step4'
+
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          {/* <Route exact path="/" />
+        <Route exact path="/pages/step1" component={Step1} /> */}
+          <Route />
+          <Route exact path="/" element={<Step1 />} />
+          <Route path="/step2" element={<Step2 />} />
+          <Route path="/step3" element={<Step3 />} />
+          <Route path="/step4" element={<Step4 />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import Typography from '@mui/material/Typography'
+
+const Header = () => {
+  return <Typography variant="h1">Header</Typography>
+}
+
+export default Header
