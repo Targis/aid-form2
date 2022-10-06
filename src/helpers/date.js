887afa1da6c8bbb125d21156633c0fb814c5ce24
Yurii Kozyk <y.kozyk@gmail.com>
2022-10-06 19:56:02 +0300

    change date format

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -3,7 +3,7 @@ import { parse, isDate } from 'date-fns'
 export function parseDateString(value, originalValue) {
   const parsedDate = isDate(originalValue)
     ? originalValue
-    : parse(originalValue, 'ddMMyyyy', new Date())
+    : parse(originalValue, 'dd.MM.yyyy', new Date())
 
   return parsedDate
 }
