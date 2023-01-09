fcbd3037074ab8dadc34521be1c55a0d070a231a
Yuri Kozyk <y.kozyk@gmail.com>
2023-01-09 11:50:56 +0200

    change message

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 const key = 'AKfycbyZMMccNvJpqQMnR6vw4aE5_XyBe1RttzCAWby9wqasH_8AjBTmJpRaAqqafXu5yruB'
 
 // HUB's queue key
-const hubKey = `AKfycbx1bruoHtGDPNI50u42GyjRICAqMN2YcSqsuiKBT7wNBtzQ8ISKBaggdJnTNCDCuQKo`
+const hubKey = `AKfycbwB-FQra8faO5kfjzaebFGh8uWAbaxXhDWRcpvgIrA6qIsg9Q0Ya-IKhPdMUFcWMT5s`
 
 
 const getActionURI = (keyString) => `https://script.google.com/macros/s/${keyString}/exec`
