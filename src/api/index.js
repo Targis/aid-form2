cec78e5fcb96c7c0e00cb9f46d55100c5b70c706
Yurii Kozyk <y.kozyk@gmail.com>
2023-01-27 17:12:54 +0200

    upd api key

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,12 @@ const key =
   'AKfycbyZMMccNvJpqQMnR6vw4aE5_XyBe1RttzCAWby9wqasH_8AjBTmJpRaAqqafXu5yruB'
 
 // HUB's queue key
-// const hubKey =
-// 'AKfycbzyPYPUxXZ3NU6AljPOR7rSi68f_pL8Fw0XstVtWOYRbNhZzReWBSNRJnI5DgYXFhTb'
+const hubKey =
+  'AKfycbzyPYPUxXZ3NU6AljPOR7rSi68f_pL8Fw0XstVtWOYRbNhZzReWBSNRJnI5DgYXFhTb'
 
 // hub's test key
-const hubKey =
-  'AKfycbyzUV6Jzs23ZINeebLJRXP3EYvCfL-oCQ8gaMHAX2Y0ybLaQGJvs3e8PgioZ0uQ6Gad'
+// const hubKey =
+//   'AKfycbyzUV6Jzs23ZINeebLJRXP3EYvCfL-oCQ8gaMHAX2Y0ybLaQGJvs3e8PgioZ0uQ6Gad'
 
 const getActionURI = (keyString) =>
   `https://script.google.com/macros/s/${keyString}/exec`
