ca2b16dce960e179eaa476b7fac724b287a2ad8d
Yurii Kozyk <y.kozyk@gmail.com>
2023-02-13 19:30:36 +0200

    add Categories section

---
+++ src/components/layout/Categories.jsx
@@ -0,0 +1,12 @@
+import Section from "components/layout/Section"
+import Typography from "@mui/material/Typography"
+
+const Categories = () => {
+  return (
+    <Section title={'Категорії'}>
+      cards
+    </Section>
+  )
+}
+
+export default Categories
