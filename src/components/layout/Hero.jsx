3e6089cf153eb42d7e13c2b5e926e05b8eb2b904
Yurii Kozyk <y.kozyk@gmail.com>
2023-03-04 17:34:11 +0200

    fix layout

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,21 +1,52 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button';
+import Section from 'components/layout/Section';
+import { styled } from '@mui/material/styles';
 import { NavLink } from "react-router-dom";
-import HowToRegIcon from '@mui/icons-material/HowToReg';
-import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import heroImage from 'img/hero-image.svg'
+
+const GridContainer = styled(Grid)(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    flexDirection: 'column'
+  },
+}));
+
+const GridImg = styled(Grid)(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    order: 1,
+    marginBottom: '32px'
+  },
+}));
+
+const GridTitle = styled(Grid)(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    order: 2
+  },
+}));
+
+const HeroTitle = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.down('lg')]: {
+    fontSize: '1.5rem',
+  },
+}));
 
 const Hero = () => {
   return (
-    <Grid container sx={{
-      py: 5,
+    <GridContainer maxWidth={'lg'} container sx={{
+      pt: 5,
+      pb: 0,
+      px: 4,
+      ml: 'auto',
+      mr: 'auto',
       minHeight: "calc(100vh - 64px)",
       display: "flex",
       alignItems: "center"
     }}>
-      <Grid item md={6}>
-        <Typography component="h1" variant="h4" sx={{ mb: 4 }}>Гуманітарна допомога <br /> внутрішньо переміщеним особам<br /> з Оріхівської громади</Typography>
+      <GridTitle item md={6}>
+        <HeroTitle component="h1" variant="h3" sx={{ mb: 4 }}>Гуманітарна допомога  внутрішньо переміщеним особам <br /> з Оріхівської громади</HeroTitle>
 
         <Button
           variant="contained"
@@ -30,12 +61,12 @@ const Hero = () => {
           Обрати категорію
         </Button>
 
-      </Grid>
-      <Grid item md={6} sx={{ px: 2, mb: 15 }}>
+      </GridTitle>
+      <GridImg item md={6} sx={{ px: 2, mb: 15 }}>
         <img src={heroImage} alt="Volonteers" style={{ width: "100%", height: "auto", filter: "grayscale(.3) contrast(0.75)" }} />
-      </Grid>
+      </GridImg>
 
-    </Grid>
+    </GridContainer>
   )
 }
 
